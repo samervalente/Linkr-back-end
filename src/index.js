2409c5462fd57e80910f3246728c896ea163c63c
Matheus Amorim <mtsgamorim@gmail.com>
2022-08-09 13:39:01 -0300

    login working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const app = express()
-app.use([cors(), express.json()])
+import loginRouter from "./routes/loginRouter.js";
 
+dotenv.config();
+const app = express();
+app.use([cors(), express.json()]);
 
-app.listen(process.env.PORT)
+app.use(loginRouter);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando na porta ${PORT}`);
+});
