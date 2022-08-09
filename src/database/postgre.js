89fa57903d229741182abddf2b24b04583505c12
samervalente <samervalente@gmail.com>
2022-08-08 21:11:02 -0300

    Initial commit

---
+++ src/database/postgre.js
@@ -0,0 +1,13 @@
+import pg from "pg"
+import dotenv from "dotenv"
+const {Pool} = pg
+dotenv.config()
+
+const connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
+})
+
+export default connection
