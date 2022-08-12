797da1914ab1977fd59d1273789ef8f183498c07
Matheus Amorim <mtsgamorim@gmail.com>
2022-08-12 17:14:38 -0300

    userFind

--- src/repository/usersRepository.js
+++ src/repository/usersRepository.js
@@ -1,35 +1,47 @@
-import connection from '../database/postgre.js';
+import connection from "../database/postgre.js";
 
 async function getUserById(userId) {
-    return connection.query(` 
+  return connection.query(
+    ` 
         SELECT * FROM users WHERE id = $1
-    `, [userId])
+    `,
+    [userId]
+  );
 }
 
 async function checkPostByUserId(postId, userId) {
-    return connection.query(`
+  return connection.query(
+    `
         SELECT posts.* FROM posts 
         JOIN users 
         ON posts."userId" = users.id 
         WHERE posts.id = $1 AND users.id = $2
-    `, [postId, userId]);
+    `,
+    [postId, userId]
+  );
 }
 
-
-
- async function SearchUsers(userName){
-    const {rows: users} = await connection.query(`SELECT id, name, "imageProfile" FROM users WHERE name ILIKE '${userName}%'
+async function SearchUsers(userName) {
+  const { rows: users } =
+    await connection.query(`SELECT id, name, "imageProfile" FROM users WHERE name ILIKE '${userName}%'
     ORDER BY name ASC    
-    LIMIT 2`)
-    return users
-  
+    LIMIT 2`);
+  return users;
+}
+
+async function FindUser(id) {
+  const { rows: users } = await connection.query(
+    `SELECT users.name FROM users WHERE id = $1`,
+    [id]
+  );
+  return users;
 }
 
 const usersRepository = {
-    getUserById,
-    checkPostByUserId,
-    SearchUsers
-     
-}
+  getUserById,
+  checkPostByUserId,
+  SearchUsers,
+  FindUser,
+};
 
 export default usersRepository;
