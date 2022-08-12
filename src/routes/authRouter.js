534d59adf230456f45f0e8ba879e52507a272e4f
Isadora Ávila <isadorgravila@gmail.com>
2022-08-12 13:40:28 -0300

    rota somente de validação de token

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import {RegisterSchema} from "../schemas/authSchema.js"
 import validateSchema from "../middlewares/schemaValidator.js";
 import { validateUserExistence } from "../middlewares/authValidator.js";
-import {RegisterUser} from "../controllers/authController.js"
+import {checkToken, RegisterUser} from "../controllers/authController.js"
+import tokenValidator from "../middlewares/tokenValidator.js";
 
 const route = Router()
 
 route.post('/signup', validateSchema(RegisterSchema), validateUserExistence, RegisterUser);
+route.post('/auth/validate', tokenValidator, checkToken);
 
 export default route;
