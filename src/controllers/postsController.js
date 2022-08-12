5bcd21f89bf9822b9738d94b81ffd623c3f281c3
Matheus Amorim <mtsgamorim@gmail.com>
2022-08-12 15:49:21 -0300

    rota user/post/:id

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,85 +1,100 @@
-import postsRepository from '../repository/postsRepository.js';
-import usersRepository from '../repository/usersRepository.js';
+import postsRepository from "../repository/postsRepository.js";
+import usersRepository from "../repository/usersRepository.js";
 import getHashtagsIds from "../utils/getHashtagIds.js";
-import urlMetadata from 'url-metadata';
+import urlMetadata from "url-metadata";
 
 export async function publishPost(req, res) {
-    const { url, text } = req.body;
+  const { url, text } = req.body;
 
-    const userId = res.locals.userId;
-    try {
-        const metadata = await urlMetadata(url);
-        const { title, image, description } = metadata;
-        
-        await postsRepository.publishUrl(url, text, userId, title, image, description);
-        return res.sendStatus(201);
+  const userId = res.locals.userId;
+  try {
+    const metadata = await urlMetadata(url);
+    const { title, image, description } = metadata;
 
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send(error);
-    }
+    await postsRepository.publishUrl(
+      url,
+      text,
+      userId,
+      title,
+      image,
+      description
+    );
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
 }
 
-export async function fetchPosts(req,res){
-    const userId = res.locals.userId;
-    try{
-        const posts = await postsRepository.fetchPosts();
-        const sendPosts = { userId: Number(userId), posts: posts.rows };
-        return res.send(sendPosts).status(200);
-
-    }catch(err){
-        
-        res.sendStatus(500);
-    }
+export async function fetchPosts(req, res) {
+  const userId = res.locals.userId;
+  try {
+    const posts = await postsRepository.fetchPosts();
+    const sendPosts = { userId: Number(userId), posts: posts.rows };
+    return res.send(sendPosts).status(200);
+  } catch (err) {
+    res.sendStatus(500);
+  }
 }
 
-export async function getTrending(req, res){
-    try {
-        const trending = await postsRepository.getTrending();
-        res.status(200).send(trending)
-    } catch (error) {
-        res.sendStatus(500)
-    }
+export async function getTrending(req, res) {
+  try {
+    const trending = await postsRepository.getTrending();
+    res.status(200).send(trending);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 }
 
-export async function getPostsByHashtag(req,res){
-    const {hashtagName} = req.params;
-    const userId = res.locals.userId;
-    try {
-        const posts = await postsRepository.getPostsByHashtagName(hashtagName);
-        const sendPosts = { userId: Number(userId), posts: posts };
-        res.status(200).send(sendPosts);
-    } catch (error) {
-        res.sendStatus(500)
-    }
+export async function getPostsByHashtag(req, res) {
+  const { hashtagName } = req.params;
+  const userId = res.locals.userId;
+  try {
+    const posts = await postsRepository.getPostsByHashtagName(hashtagName);
+    const sendPosts = { userId: Number(userId), posts: posts };
+    res.status(200).send(sendPosts);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+}
+
+export async function getPostsByUserId(req, res) {
+  const param = req.params.id;
+  const userId = res.locals.userId;
+  try {
+    const posts = await postsRepository.getPostsByUserId(param);
+    const sendPosts = { userId: Number(userId), posts: posts };
+    res.status(200).send(sendPosts);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 }
 
 export async function updatePost(req, res) {
-    const userId = res.locals.userId;
-    const { text } = req.body;
-    const { id: postId } = req.params;
-    try {
-        //conferir se o post é do usuário
-        const result = await usersRepository.checkPostByUserId(postId, userId);
-        if (!result.rowCount) {
-            return res.status(401).send('Esse post não pertence ao usuário logado');
-        }
-
-        //apagar relação hashtagsposts
-        await postsRepository.deleteHashtagsPosts(postId);
-
-        //atualizar posts
-        await postsRepository.updatePostById(text, postId);
-
-        //inserir relações hashtagsposts novamente
-        if(text) {
-            const hashtags = getHashtagsIds(text);
-            await postsRepository.insertHashtags(hashtags, postId);
-            return res.sendStatus(200);
-        }
-        return res.sendStatus(200);
-
-    } catch (error) {
-        res.sendStatus(500);
+  const userId = res.locals.userId;
+  const { text } = req.body;
+  const { id: postId } = req.params;
+  try {
+    //conferir se o post é do usuário
+    const result = await usersRepository.checkPostByUserId(postId, userId);
+    if (!result.rowCount) {
+      return res.status(401).send("Esse post não pertence ao usuário logado");
     }
+
+    //apagar relação hashtagsposts
+    await postsRepository.deleteHashtagsPosts(postId);
+
+    //atualizar posts
+    await postsRepository.updatePostById(text, postId);
+
+    //inserir relações hashtagsposts novamente
+    if (text) {
+      const hashtags = getHashtagsIds(text);
+      await postsRepository.insertHashtags(hashtags, postId);
+      return res.sendStatus(200);
+    }
+    return res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 }
--- src/repository/postsRepository.js
+++ src/repository/postsRepository.js
@@ -1,72 +1,88 @@
-import connection from '../database/postgre.js';
+import connection from "../database/postgre.js";
 import getHashtagsIds from "../utils/getHashtagIds.js";
 
 async function publishUrl(url, text, userId, title, image, description) {
-  
-    const {rows: result} = await connection.query(` 
+  const { rows: result } = await connection.query(
+    ` 
     INSERT INTO posts (url, description, "userId", "urlTitle", "urlImage", "urlDescription") 
     VALUES ($1, $2, $3, $4, $5, $6) 
     RETURNING *
-`, [url, text, userId, title, image, description]);
+`,
+    [url, text, userId, title, image, description]
+  );
 
-if (!result[0].description) {
-    return
+  if (!result[0].description) {
+    return;
+  }
+  const hashtags = getHashtagsIds(result[0].description);
+  const postId = result[0].id;
+  await insertHashtags(hashtags, postId);
 }
-    const hashtags = getHashtagsIds(result[0].description);
-    const postId = result[0].id;
-    await insertHashtags(hashtags, postId);
-   }
 
-async function fetchPosts(){
-    return connection.query(`
+async function fetchPosts() {
+  return connection.query(`
         SELECT posts.*, users."imageProfile", users.name
         FROM posts JOIN users ON posts."userId" = users.id
         ORDER BY "createdAt" DESC 
         LIMIT 20
-    `)
+    `);
 }
 
-async function insertHashtags(hashtags, postId){
-    hashtags.map(async hashtag => {
-        const {rows: hashtagOnDB} = await connection.query(`
+async function insertHashtags(hashtags, postId) {
+  hashtags.map(async (hashtag) => {
+    const { rows: hashtagOnDB } = await connection.query(
+      `
             SELECT * FROM hashtags 
             WHERE name = $1
-        `,[hashtag.toLowerCase()]);
+        `,
+      [hashtag.toLowerCase()]
+    );
 
-        if (hashtagOnDB[0]) {
-            await connection.query(`
+    if (hashtagOnDB[0]) {
+      await connection.query(
+        `
                 INSERT INTO hashtagsposts ("postId", "hashtagId") 
                 VALUES ($1, $2)
-            `,[postId, hashtagOnDB[0].id]);
-        } else {
-            const {rows: result} = await connection.query(`
+            `,
+        [postId, hashtagOnDB[0].id]
+      );
+    } else {
+      const { rows: result } = await connection.query(
+        `
                 INSERT INTO hashtags (name) 
                 VALUES ($1)
                 RETURNING *
-            `, [hashtag.toLowerCase()]);
+            `,
+        [hashtag.toLowerCase()]
+      );
 
-            await connection.query(`
+      await connection.query(
+        `
             INSERT INTO hashtagsposts ("postId", "hashtagId") 
             VALUES ($1, $2)
-        `,[postId, result[0].id]);
-        }
-    }) 
+        `,
+        [postId, result[0].id]
+      );
+    }
+  });
 }
 
-async function getTrending(){
-    const {rows: trending} = await connection.query(`SELECT h.id, COUNT("postId") as quantPosts, h.name as tag FROM hashtagsposts
+async function getTrending() {
+  const { rows: trending } =
+    await connection.query(`SELECT h.id, COUNT("postId") as quantPosts, h.name as tag FROM hashtagsposts
     JOIN hashtags h
     ON h.id = "hashtagId"
     GROUP BY "hashtagId", h.name, h.id
     ORDER BY quantposts DESC
     LIMIT 10
-    `)
+    `);
 
-    return trending
+  return trending;
 }
 
-async function getPostsByHashtagName(name){
-    const {rows: posts} = await connection.query(`SELECT p.*, users.name as name, users."imageProfile"
+async function getPostsByHashtagName(name) {
+  const { rows: posts } = await connection.query(
+    `SELECT p.*, users.name as name, users."imageProfile"
 
     FROM hashtagsposts h
     JOIN posts p
@@ -76,34 +92,56 @@ async function getPostsByHashtagName(name){
     JOIN users
     ON users.id = "userId"
     WHERE ht.name = $1 
-    ORDER BY p."createdAt" DESC`,[name])
-    
-    return posts;
+    ORDER BY p."createdAt" DESC`,
+    [name]
+  );
+
+  return posts;
+}
+
+async function getPostsByUserId(userId) {
+  const { rows: posts } = await connection.query(
+    `SELECT posts.*, users.name as name, users."imageProfile"
+    FROM posts
+    JOIN users
+    ON posts."userId" = users.id
+    WHERE posts."userId" = $1
+    ORDER BY posts."createdAt" DESC`,
+    [userId]
+  );
+  return posts;
 }
 
 async function deleteHashtagsPosts(postId) {
-    return connection.query(`
+  return connection.query(
+    `
         DELETE FROM hashtagsposts 
         WHERE "postId" = $1;
-    `, [postId]);
+    `,
+    [postId]
+  );
 }
 
 async function updatePostById(text, postId) {
-    return connection.query(`
+  return connection.query(
+    `
         UPDATE posts 
         SET description = $1 
         WHERE id = $2;
-    `, [text, postId]);
+    `,
+    [text, postId]
+  );
 }
 
 const postsRepository = {
-    publishUrl, 
-    fetchPosts,
-    insertHashtags,
-    getTrending,
-    getPostsByHashtagName,
-    deleteHashtagsPosts,
-    updatePostById
-}
+  publishUrl,
+  fetchPosts,
+  insertHashtags,
+  getTrending,
+  getPostsByHashtagName,
+  deleteHashtagsPosts,
+  updatePostById,
+  getPostsByUserId,
+};
 
 export default postsRepository;
